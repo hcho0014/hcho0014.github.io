196cfe59c494a1989998d6f5b9712883aea9a89a
hcho00121404 <hcho00121404@gmail.com>
2017-03-16 00:46:59 -0400

    hi

--- battleship.js
+++ battleship.js
@@ -2,17 +2,18 @@ var numberofships = 0;
 function makeMove(id,player) {
  if(player=="player"){
   if(numberofships < 10){
+      document.getElementById(id).style.color="black";
      document.getElementById(id).innerHTML="#";
     numberofships++; 
   }
  }
   else{
-    alert("aibox");
-    alert("insidebox of " + id + "contents " + document.getElementById(id).innerHTML);
     if(document.getElementById(id).innerHTML=="x"){
       document.getElementById(id).style.color="red";
+      alert("You hit a boat")
     }
     else{
+      document.getElementById(id).style.color="green"
       document.getElementById(id).innerHTML="O";
       alert("You failed to hit the boat")
 
