a2ec328e43d8d1fe3a4d70d4cc7e01c3c627df7a
hcho00121404 <hcho00121404@gmail.com>
2017-03-15 23:03:37 -0400

    jessu

--- battleship.js
+++ battleship.js
@@ -1,10 +1,17 @@
 var numberofships = 0;
 function makeMove(id,player) {
+ if(player=="player"){
   if(numberofships < 10){
      document.getElementById(id).innerHTML="#";
-    numberofships++;
-    
-  } 
- 
+    numberofships++; 
+  }
+ }
+  else{
+    alert("aibox");
+    alert("insidebox of " + id + "contents " + document.getElementById(id).innerHTML);
+    if(document.getElementById(id).innerHTML=="x"){
+      document.getElementById(id).style.color="red";
+    }
+  }
     
 }
