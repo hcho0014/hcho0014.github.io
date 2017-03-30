f002f3fda82b5f9568f80a9c9a4566e82d04ec8e
hcho00121404 <hcho00121404@gmail.com>
2017-03-29 22:01:44 -0400

    hi

--- battleship.js
+++ battleship.js
@@ -1,4 +1,5 @@
 var numberofships = 0;
+
 function makeMove(id,player) {
  if(player=="player"){
   if(numberofships < 10){
@@ -8,7 +9,7 @@ function makeMove(id,player) {
   }
  }
   else{
-    if(document.getElementById(id).innerHTML=="x"){
+    if(document.getElementById(id).innerHTML=="X"){
       document.getElementById(id).style.color="red";
       alert("You hit a boat")
     }
@@ -20,4 +21,41 @@ function makeMove(id,player) {
      }
     }
     
+}
+function getRandomNum(){
+  return Math.floor((Math.random()*10)+1);
+}
+function placeAiShip(ship){
+  document.getElementById(ship).innerHTML="X";
+}
+function generateAiShips(){
+ var ship1="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship2="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship3="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship4="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship5="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship6="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship7="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship8="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship9="row"+getRandomNum()+"col"+getRandomNum()+"a";
+ var ship10="row"+getRandomNum()+"col"+getRandomNum()+"a";
+   placeAiShip(ship1);
+   placeAiShip(ship2);
+   placeAiShip(ship3);
+   placeAiShip(ship4);
+   placeAiShip(ship5);
+   placeAiShip(ship6);
+   placeAiShip(ship7);
+   placeAiShip(ship8);
+   placeAiShip(ship9);
+   placeAiShip(ship10);
+
+}
+function placeAimis(mis){
+   document.getElementById(mis).innerHTML="K";
+}
+function generateAimis(){
+   var mis1="row"+getRandomNum()+"col"+getRandomNum();
+   placeAimis(mis1);
+  alert(mis1)
 }
